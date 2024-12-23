4dc662f2b1c2dcd7a0de192ce555ada4bfa80eef
sterlker <hazimqayyim1102@gmail.com>
2024-12-23 16:19:11 +0800

    Minor changes

---
+++ lib/pages/notes_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class NotesPage extends StatefulWidget {
+  const NotesPage({super.key});
+
+  @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(
+          'Notes'
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
